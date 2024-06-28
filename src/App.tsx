8f521c2944856f86c8f46da16da0d60c13cf13e7
una3325 <a33256448@gmail.com>
2024-06-29 04:39:01 +0900

    sort 로직 생성 - task 이동

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ import ListsContainer from './components/ListsContainer/ListsContainer';
 import { useTypedDispatch, useTypedSelector } from './hooks/redux';
 import EditModal from './components/EditModal/EditModal';
 import LoggerModal from './components/LoggerModal/LoggerModal';
-import { deleteBoard } from './store/slices/boardsSlice';
+import { deleteBoard, sort } from './store/slices/boardsSlice';
 import { v4 } from 'uuid';
 import { addLog } from './store/slices/loggerSlice';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 function App() {
   const dispatch = useTypedDispatch();
@@ -59,6 +59,46 @@ function App() {
 
   const handleDragEnd = (result: any) => {
     console.log(result);
+
+    const { destination, source, draggableId } = result;
+    console.log('lists', lists);
+
+    const sourceList = lists.filter(
+      (list) => list.listId === source.droppableId
+    )[0];
+
+    console.log('source list', sourceList);
+
+    dispatch(
+      sort({
+        boardIndex: boards.findIndex(
+          (board) => board.boardId === activeBoardId
+        ),
+        droppableIdStart: source.droppableId,
+        droppableIdEnd: destination.droppableId,
+        droppableIndexStart: source.index,
+        droppableIndexEnd: destination.index,
+        draggableId,
+      })
+    );
+
+    dispatch(
+      addLog({
+        logId: v4(),
+        logMessage: `
+    리스트 "${sourceList.listName}"에서
+    리스트 "${
+      lists.filter((list) => list.listId === destination.droppableId)[0]
+        .listName
+    }"으로
+    "${
+      sourceList.tasks.filter((task) => task.taskId === draggableId)[0].taskName
+    }"을 올김.
+    `,
+        logAuthor: 'User',
+        logTimestamp: String(Date.now()),
+      })
+    );
   };
 
   return (
--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -38,6 +38,15 @@ type TDeleteBoardAction = {
   boardId: string;
 };
 
+type TSortAction = {
+  boardIndex: number;
+  droppableIdStart: string;
+  droppableIdEnd: string;
+  droppableIndexStart: number;
+  droppableIndexEnd: number;
+  draggableId: string;
+};
+
 const initialState: TBoardsState = {
   modalActive: false,
   boardArray: [
@@ -177,10 +186,38 @@ const boardsSlice = createSlice({
     setModalActive: (state, { payload }: PayloadAction<boolean>) => {
       state.modalActive = payload;
     },
+
+    sort: (state, { payload }: PayloadAction<TSortAction>) => {
+      //same list
+      if (payload.droppableIdStart === payload.droppableIdEnd) {
+        const list = state.boardArray[payload.boardIndex].lists.find(
+          (list) => list.listId === payload.droppableIdStart
+        );
+
+        //변경 시키는 아이템을 배열에서 지워줍니다.
+        //return값으로 지워진 아이템을 잡아줍니다.
+        const card = list?.tasks.splice(payload.droppableIndexStart, 1);
+        list?.tasks.splice(payload.droppableIndexEnd, 0, ...card!);
+      }
+
+      //other list
+      if (payload.droppableIdStart !== payload.droppableIdEnd) {
+        const listStart = state.boardArray[payload.boardIndex].lists.find(
+          (list) => list.listId === payload.droppableIdStart
+        );
+
+        const card = listStart!.tasks.splice(payload.droppableIndexStart, 1);
+        const listEnd = state.boardArray[payload.boardIndex].lists.find(
+          (list) => list.listId === payload.droppableIdEnd
+        );
+        listEnd?.tasks.splice(payload.droppableIndexEnd, 0, ...card);
+      }
+    },
   },
 });
 
 export const {
+  sort,
   deleteBoard,
   addBoard,
   deleteList,
